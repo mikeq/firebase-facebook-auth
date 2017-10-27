e7ddbdfaaff8baf1e1e56c8f1b15f46a095fd645
Mike Quinn <mike@velochallenge.org>
2017-10-27 12:53:46 +0100

    basic layout and authenticate against facebook

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import Button from 'material-ui/Button';
 import Icon from 'material-ui/Icon';
 import LockOpen from 'material-ui-icons/LockOpen';
 import Lock from 'material-ui-icons/Lock';
+import firebase from 'firebase';
 
 import './App.css';
 import NavBar from './components/header/NavBar';
+import config from './config/config';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+
+    firebase.initializeApp(config);
+  }
+
+  login() {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(resp => console.log(resp))
+      .catch(error => console.log(error));
+  }
+
+  logout() {
+    firebase.auth().signOut();
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -16,14 +40,14 @@ class App extends Component {
         <NavBar />
         <div className="grid">
           <div>
-            <Button raised color="accent">
+            <Button raised color="accent" onClick={this.login}>
               <Icon>
                 <LockOpen />
               </Icon>Login
             </Button>
           </div>
           <div>
-            <Button raised color="accent">
+            <Button raised color="accent" onClick={this.logout}>
               <Icon>
                 <Lock />
               </Icon>Logout
